3e73af5226fd55bfbf068ee0dd283b35c8497367
Beaney <matt.beaney@pagesuite.co.uk>
2017-09-22 18:34:18 +0100

    Mostly just guessing now, here goes nothing!

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
 	$code = $_GET['code'];
 	$state = $_GET['state'];
-	header('Location: https://todo-app-live-75be0.firebaseapp.com/__/auth/handler?code=' . $code . '&state=' . $state);
+	header('Location: https://todo-app-live-75be0.firebaseapp.com/__/auth/handler');
 ?>
