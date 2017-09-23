6f49f8529ef75bf1072513dd12fd86bc6fa05767
Beaney <matt.beaney@pagesuite.co.uk>
2017-09-23 19:33:49 +0100

    Added PHP script to perform POST request

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php 
-	$code = $_GET['code'];
-	$state = $_GET['state'];
-	//header('Location: https://todo-app-live-75be0.firebaseapp.com/__/auth/handler?code=' . $code);
+<?php
+	$code = $_GET["code"];
+	$url = 'https://github.com/login/oauth/access_token';
+	$data = array('client_id' => 'df6b508bc92edc6cdb8c', 'client_secret' => '819dc555f87766b8cdb8f3974c18bbd42b69495f', 'code' => $code);
+
+	// use key 'http' even if you send the request to https://...
+	$options = array(
+	    'http' => array(
+	        'header'  => "Content-type: application/json",
+	        'header'  => "Accept: application/json",
+	        'method'  => 'POST',
+	        'content' => http_build_query($data)
+	    )
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	if ($result === FALSE) { /* Handle error */ }
+	$data = json_decode($result);
+	header('Location: http://mb-react-todo.herokuapp.com/?accessToken=' . $data->access_token);
 ?>
